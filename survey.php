c245348a33025f862bd9881eeb589fa0985e94ab
John Sawers <johnrsawers@gmail.com>
2013-12-16 13:30:15 +0000

    Added survey page

---
+++ survey.php
@@ -0,0 +1,26 @@
+<?php 
+$pageTitle="Customer survey";
+$pageDesc="Customer survey"; 
+include('inc/head.php'); ?>
+  <?php 
+include('inc/nav.php'); ?>
+  <div class="w-container program-page">
+      <div class="pd-embed" id="pd_1387197700"></div>
+<script type="text/javascript">
+  var _polldaddy = [] || _polldaddy;
+
+  _polldaddy.push( {
+    type: "iframe",
+    auto: "1",
+    domain: "myteamactive.polldaddy.com/s/",
+    id: "myteam",
+    placeholder: "pd_1387197700"
+  } );
+
+  (function(d,c,j){if(!document.getElementById(j)){var pd=d.createElement(c),s;pd.id=j;pd.src='http://i0.poll.fm/survey.js';s=document.getElementsByTagName(c)[0];s.parentNode.insertBefore(pd,s);}}(document,'script','pd-embed'));
+</script>
+  </div>
+  <?php 
+include('inc/footer.php'); ?>
+</body>
+</html>
